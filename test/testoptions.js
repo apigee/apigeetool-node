9fedb8a0ecaf371be900afd8ba6a433ec21d5839
Kevin Shomper <kjshomper@gmail.com>
2019-09-12 13:15:53 -0400

    unit tests for keyfile/certfile option additions

--- test/testoptions.js
+++ test/testoptions.js
@@ -2,8 +2,21 @@ var assert = require('assert');
 var util = require('util');
 
 var options = require('../lib/options');
+var defaults = require('../lib/defaults');
+var fs = require('fs');
+var request = require('request');
 
-describe('Options parsing test', function(done) {
+describe('Options parsing test', function() {
+  beforeAll(function () {
+    spyOn(fs, 'readFileSync').and.returnValue(Buffer.from('abc'));
+    spyOn(request, 'defaults');
+  });
+
+  afterEach(function () {
+    fs.readFileSync.calls.reset();
+    request.defaults.calls.reset();
+  });
+
   it('Test no descriptor', function(done) {
     var desc = {};
     var opts = { foo: 1, bar: 'baz'};
@@ -118,6 +131,77 @@ describe('Options parsing test', function(done) {
     assert.equal(opts.foo, 'bar');
     assert.equal(opts.toggle, true);
   });
+
+  // the prior test demonstrates the ability to set any command
+  // line switch, with a value or as a boolean; so, this test
+  // will validate the switches are acted upon correctly
+  it('Test setting the --keyfile/--certfile options', function() {
+    // set up starting variables
+    var desc = {
+      keyfile: { shortOption: 'K', name: 'Key file' },
+      certfile: { shortOption: 'C', name: 'Cert file' },
+    };
+    var argv = [ '-K', 'key.pem', '--certfile', 'cert.pem' ];
+    // initiate spys
+    // spyOn(fs, 'readFileSync').and.returnValue(Buffer.from('abc'));
+    // spyOn(request, 'defaults');
+
+    var opts = options.getopts(argv, 0, desc);
+    // validate the short option is working
+    assert.equal(opts.keyfile, 'key.pem');
+    // validate the long option is working
+    assert.equal(opts.certfile, 'cert.pem');
+
+    // validate the changed defaults.js code reads the .pem files
+    // and sets up the request options object with the file buffers
+    var rqst = defaults.defaultRequest(opts);
+    // validate the files is read when the options is set
+    expect(fs.readFileSync).toHaveBeenCalledWith('key.pem');
+    expect(fs.readFileSync).toHaveBeenCalledWith('cert.pem');
+    // validate the request's default request object is constructed with key/cert buffers
+    expect(request.defaults).toHaveBeenCalledWith(
+      {          
+        'auth': {},
+        'json': true,
+        'headers': {},
+        'agentOptions': {},
+        'key': Buffer.from('abc'),
+        'cert': Buffer.from('abc')
+      });
+  });
+
+  // negative case, i.e. the additional options are not included as
+  // part of the command line switches
+  it('Test omitting the --keyfile/--certfile options', function() {
+    // set up starting variables
+    var desc = {
+      keyfile: { shortOption: 'K', name: 'Key file' },
+      certfile: { shortOption: 'C', name: 'Cert file' },
+    };
+    var argv = [ ];
+    // initiate spys
+    // spyOn(fs, 'readFileSync').and.returnValue(Buffer.from('abc'));
+    // spyOn(request, 'defaults');
+
+    var opts = options.getopts(argv, 0, desc);
+    // validate the options were not set
+    assert.equal(opts.keyfile, undefined);
+    assert.equal(opts.certfile, undefined);
+
+    // validate the undefined options don't attempt a file read
+    var rqst = defaults.defaultRequest(opts);
+    // validate the files is read when the options is set
+    expect(fs.readFileSync).not.toHaveBeenCalled();
+    // validate the request's default request object absent the key and cert properties
+    expect(request.defaults).toHaveBeenCalledWith(
+      {          
+        'auth': {},
+        'json': true,
+        'headers': {},
+        'agentOptions': {},
+      });
+  });
+
   it('Test secure value', function() {
     var sv = new options.SecureValue('foobar');
     assert.notEqual(util.format('%s', sv), 'foobar');
@@ -125,4 +209,5 @@ describe('Options parsing test', function(done) {
     assert.equal(sv.getValue(), 'foobar');
     assert(sv instanceof options.SecureValue);
   });
+
 });
